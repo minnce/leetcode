7a85c5cbd3cff56f3a676cacb2f0303c5ca178ce
minnce <105378107+minnce@users.noreply.github.com>
2022-09-30 13:29:40 -0500

    Create validPalindrome.py

---
+++ validPalindrome.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        listS = list(s.upper())
+        counter = 0
+        for x in range(0,len(listS)):
+            if listS[x+counter].isalnum() == False:
+                del listS[x+counter]
+                counter-=1
+        for x in range(0,len(listS)):
+            if listS[x] != listS[-(x+1)]:
+                return False
+        return True
